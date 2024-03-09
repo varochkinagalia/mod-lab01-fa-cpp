fb04b49540edde5b4f5f039418bda9b654af7f19
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-03-09 21:48:21 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -11,7 +11,7 @@ unsigned int faStr1(const char *str) {
         if (str[i] != ' ') {
             inWord = true;
         }
-        if (str[i] == '0' || str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9') {
+        if (str[i] >= '0' && str[i] <= '9') {
             cifra = true;
         }
         if (str[i] == ' ' && inWord == true) {
