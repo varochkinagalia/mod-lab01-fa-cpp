40a7bb4afc07df4d019f251b48def7569aec84bc
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-03-09 22:39:24 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,88 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    bool inWord = false;
+    bool cifra = false;
+    int count = 0;
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ') {
+            inWord = true;
+            if (str[i] >= '0' && str[i] <= '9') {
+                cifra = true;
+            }
+            else if (str[i] == ' ' && inWord == true) {
+                if (cifra == false) {
+                    count++;
+                }
+                inWord = false;
+                cifra = false;
+            }
+        }
+        i++; 
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    bool first = true;//первая буква
+    bool correct = false;
+    bool second = false;
+    int count = 0;
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ') {
+            correct = false;
+            if (str[i] >= 'A' && str[i] <= 'Z' && first == true) {
+                correct = true;
+                first = false;
+            }
+            else if (str[i] >= 'a' && str[i] <= 'z') {
+                first = false;
+                second = true;
+            }
+            else {
+                if (correct == true && second == true) {
+                    count++;
+                }
+                first = true;
+                correct = false;
+                second = false;
+            }
+        }
+        i++;
+    }
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    bool inWord = false;
+    int count = 0;
+    int len = 0;
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && inWord == false) {
+            inWord = true;
+            count++;
+        }
+        else if (str[i] != ' ') {
+            len++;
+        }
+        else if (str[i] == ' ' && inWord == true) {
+            inWord = false;
+        }
+        i++;
+    }
+    float ans = static_cast<float>(len) / count;
+    if (count == 0){
+        return 0;
+    }
+    else {
+        if ((ans - static_cast<int>(ans)) >= 0.5) {
+            return static_cast<int>(ans) + 1;
+        }
+        else {
+            return static_cast<int>(ans);
+        }
+    }
 }
